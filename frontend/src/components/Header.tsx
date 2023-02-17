939133b07702a2f69be19b66bf21ef10bfa4307c
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-02-17 18:12:22 -0300

    avanced header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <div>texto</div>
-      <div>img</div>
+    <header className="header">
+      <div className="header__divText">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+        cumque veniam veritatis placeat delectus eaque doloremque inventore
+        ipsa, molestiae dignissimos omnis nobis, sint quisquam mollitia itaque!
+        Quod unde sit quasi.
+      </div>
+      <div className="header__divImg">
+        <Image
+          className="header__img"
+          src="/imgcubo.png"
+          width={300}
+          height={300}
+          alt="img cubo"
+        />
+      </div>
     </header>
   );
 };
